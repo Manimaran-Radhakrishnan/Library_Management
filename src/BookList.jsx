0baae2488c2f8169a393ae8b8f78f8e756413ab8
Manimaran-Radhakrishnan <manimaran8491@gmail.com>
2023-09-22 12:36:40 +0530

    Library

---
+++ src/BookList.jsx
@@ -0,0 +1,61 @@
+import { Book } from "./Book";
+import { useState } from "react";
+import { useEffect } from "react";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { useNavigate } from "react-router-dom";
+
+export function BookList() {
+  const navigate = useNavigate();
+  const [book, setbook] = useState([]);
+
+  const getbook = () => {
+    fetch("https://650be2f547af3fd22f66a1ea.mockapi.io/library", {
+      method: "GET",
+    })
+      .then((data) => data.json())
+      .then((data) => setbook(data));
+  };
+
+  useEffect(() => getbook(), []);
+
+  const deletebook = (id) => {
+    console.log("deleting the Book,id");
+    fetch(` https://650be2f547af3fd22f66a1ea.mockapi.io/library/${id}`, {
+      method: "DELETE",
+    }).then(() => getbook());
+  };
+
+  return (
+    <div>
+      <div className="book-list">
+        {book.map((data) => (
+          <Book
+            key={data.id}
+            book={data}
+            id={data.id}
+            deleteButton={
+              <IconButton
+                sx={{ marginLeft: "auto" }}
+                color="error"
+                onClick={() => deletebook(data.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+            editButton={
+              <IconButton
+                sx={{ marginLeft: "auto" }}
+                color="secondary"
+                onClick={() => navigate(`/books/edit/${data.id}`)}
+              >
+                <EditIcon />
+              </IconButton>
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <div className="home">
+      <h1>🔖 Welcome to our Library 🔖 </h1>
+      <img
+        id="home"
+        src="https://libraryhub.in/wp-content/uploads/2023/02/siver750x500-IMG_7285-750x500-1.jpg"
+        alt="library"
+      />
+    </div>
+  );
+}
